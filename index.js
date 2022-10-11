f77040356ed1401ca3026c0791cdef9088bfeaf6
Leonam Bernini da Silva <leonamberninidasilva@MBP-de-Leonam.local>
2022-10-10 23:26:08 -0300

    get customer

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/valid-customer", (req, res) => {
-    return res.json({success: true});
+app.get("/valid-customer/:customer", async (req, res) => {
+
+    try {
+        const customer_id = req.params.customer;
+    
+        if( !customer_id ){
+            return res.json({error: true, msg: 'ID do cliente não informado.'});
+        }
+        
+        var config = {
+            method: 'get',
+            url: `https://api.nuvemshop.com.br/v1/${process.env.USER}/customers/${customer_id}?fields=note`,
+            headers: { 
+                'Authentication': `bearer ${process.env.TOKEN}`
+            }
+        };
+    
+        const result = await axios(config);
+            
+        if( !result || typeof result !== 'object' ){
+            return res.json({error: true, msg: 'Erro ao consultar cliente.'});
+        }
+
+        if( 'note' in result  ){
+            const { note } = result;
+            if( !note || note.trim() === '' ){
+                return res.json({success: true, msg: 'Cliente normal', is_special: false});
+            }else if( note.indexOf('CLIENTE_VIP') >= 0 ){
+                return res.json({success: true, msg: 'Cliente especial', is_special: true});
+            }
+        }
+    
+        return res.json({error: true, msg: 'Nenhum resultado encontrado.'});
+    } catch (error) {
+        return res.json({error: true, msg: `Erro ao consultar cliente: ${error}` });
+    }
 });
 
 app.listen(port, () => {
